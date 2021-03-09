2edd5b433c9899a5b480c845f5b9b477aed0f759
Hetizz <heta.huostila@gmail.com>
2021-03-09 19:18:49 +0200

    Luontoreitit kartalle

---
+++ js/luontoreitit.js
@@ -0,0 +1,40 @@
+'use strict';
+
+let map = L.map('map').setView([60.194392, 24.977437], 12);
+
+function piirraKartta() {
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+}
+
+let haku = 'https://citynature.eu/api/wp/v2/places?cityid=5';
+
+function haeSpotit() {
+  fetch(haku)
+  .then(function(vastaus){
+    return vastaus.json();
+  }).then(function(json) {
+    console.log(json);
+    paikatKartalle(json);
+  })
+
+  function paikatKartalle(json) {
+    console.log(json[0]);
+    console.log(json[0].title);
+    console.log(json[0].points[0].locationPoint.lat);
+    console.log(json[0].points[0].locationPoint.lng);
+
+    for (let i = 0; i < json.length; i++) {
+      let lat = json[i].points[0].locationPoint.lat;
+      let lng = json[i].points[0].locationPoint.lng;
+      let title = json[i].title;
+
+      L.marker([lat, lng]).addTo(map)
+      .bindPopup(title);
+    }
+  }
+}
+
+piirraKartta();
+haeSpotit();
--- js/sisallonpaivitys.js
+++ js/sisallonpaivitys.js
@@ -44,3 +44,23 @@ function toggleAsemat() {
   script.src= 'js/bussiasemat.js';
   head.appendChild(script);
 }
+
+function toggleSpotit() {
+  const main = document.querySelector("main"); //Merkitään main-elementti muistiin.
+  document.getElementById("article").remove();
+
+  let article = document.createElement('article'); //Luodaan uusi article ja syötetään siihen pohjustavat tiedot.
+  article.setAttribute("id", "article");
+  const art = `<div id="map" style="width: 100%; height: 100%;"></div>
+                  <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"><\/script>`;
+
+  article.innerHTML = art;
+  main.appendChild(article);
+
+  console.log('luontoreitit');
+
+  let head = document.getElementsByTagName('head')[0]; //Luodaan uusi skripti, joka luotaessa ajaa sen sisällön.
+  let script= document.createElement('script');
+  script.src = 'js/luontoreitit.js';
+  head.appendChild(script);
+}
